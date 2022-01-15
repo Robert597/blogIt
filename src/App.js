c619e047d6bc104780c5acbdd28674117f5c78f1
Robert597 <robertseun1@gmail.com>
2022-01-15 09:21:53 -0800

    just made few changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [starting, setStarting] = useState(true);
+  window.onload = () => {
+     setTimeout(() => {
+      setStarting(false);
+      console.log(starting);
+    }, 4000);
+   }
+   console.log(starting);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     { starting && <div className="intro"> okay </div>}
+     {!starting && <p>odbowboiw</p>}
     </div>
   );
 }
