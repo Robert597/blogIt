c8975489d930e2aeb4db0d39386a6380a36c1b66
Robert597 <robertseun1@gmail.com>
2022-02-09 03:03:41 +0100

    adding footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,15 @@
+import { FaInstagram, FaTwitter, FaFacebook } from "react-icons/fa";
 const Footer = () => {
     return (
         <div className="footer">
-            BLogit
+             <div className='FooterIcon'>
+        <FaInstagram className='footerIcon'/>
+        <FaFacebook className='footerIcon'/>
+        <FaTwitter className='footerIcon'/>
+    </div>
+    <div className='FooterText'>
+      <p>&copy; 2022 Robert Oluwaseun</p>
+    </div>
         </div>
     )
 }
